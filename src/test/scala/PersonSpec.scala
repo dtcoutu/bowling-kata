43eba5043d62a913e60f2124b5526187bf0d5880
David Coutu <dcoutu@covermymeds.com>
2021-07-15 11:52:03 -0400

    Fix broken test

--- src/test/scala/PersonSpec.scala
+++ src/test/scala/PersonSpec.scala
@@ -3,8 +3,8 @@ import org.scalatest.FunSuite
 import java.util.{Date, GregorianCalendar}
 
 class PersonSpec extends FunSuite {
-  test("the name is set correctly in constructor") {
+  test("the first name is set correctly in constructor") {
     val p = new Person("Barney", "Rubble")
-    assert(p.firstName == "Barney Rubble")
+    assert(p.firstName == "Barney")
   }
 }
